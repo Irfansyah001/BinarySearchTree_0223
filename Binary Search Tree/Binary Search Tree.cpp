283ce5c7a51ffa98c8ce0e5cb20c48944e96703c
Irfansyah001 <syahirfan545@gmail.com>
2024-06-13 15:40:30 +0700

    add if the new node is less than the parent

--- Binary Search Tree/Binary Search Tree.cpp
+++ Binary Search Tree/Binary Search Tree.cpp
@@ -34,8 +34,12 @@ public:
 		if (parent == nullptr) {    //if the tree is empty
 			ROOT = newNode;  //set the new node as the root
 			return;
+		}
+
+		if (element < parent->info) { //if the new node is less than the parent
+
+		}
 	}
-}
 };
 
 int main()
